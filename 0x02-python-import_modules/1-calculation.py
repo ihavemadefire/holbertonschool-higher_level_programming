9bbc0b2220a8a80c873b7b4a6b241870e8024728
Jacob Ide <jacobmeide@gmail.com>
2020-04-30 17:07:52 +0000

    1,1

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python3
-from calculator_1 import add, sub, mul, div
-# define vars #
-a = 10
-b = 5
-# print funcs #
-print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
-print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
-print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
-print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
+
+def finalcalc():
+    # define vars #
+    a = 10
+    b = 5
+    # print funcs #
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+    print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
+    print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
+    print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
+
+if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
+    finalcalc()
