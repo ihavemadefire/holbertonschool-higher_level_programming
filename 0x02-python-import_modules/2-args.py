8738e17faa8318d3b634c990c9a56284d65bb47b
Jacob Ide <jacobmeide@gmail.com>
2020-04-30 20:11:57 +0000

    2,1

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
 
-import sys
+    import sys
 
-l = len(sys.argv)
+    l = len(sys.argv)
 
-if l == 1:
-    print("0 arguments.")
-elif l == 2:
-    print("1 argument:")
-    print("1: {}".format(sys.argv[1]))
-else:
-    print("{:d} arguments:".format(l-1))
-    for i in range(1, l):
-        print("{:d}: {}".format(i, sys.argv[i]))
+    if l == 1:
+        print("0 arguments.")
+    elif l == 2:
+        print("1 argument:")
+        print("1: {}".format(sys.argv[1]))
+    else:
+        print("{:d} arguments:".format(l-1))
+        for i in range(1, l):
+            print("{:d}: {}".format(i, sys.argv[i]))
