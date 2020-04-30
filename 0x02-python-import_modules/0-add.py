9ef8d3811216b87ae051a96835915a4d6cde1bfb
Jacob Ide <jacobmeide@gmail.com>
2020-04-30 16:48:09 +0000

    0,1

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
 from add_0 import add
-# Assigns variables #
-a = 1
-b = 2
-# prints the addition of the variables #
-print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+def addvars():
+    # Assigns variables #
+    a = 1
+    b = 2
+    # prints the addition of the variables #
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+
+
+if __name__ == "__main__":
+    addvars()
