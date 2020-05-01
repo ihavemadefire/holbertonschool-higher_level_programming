007c7eeaf6f1ff36122a17243be4468c19a7c1da
Jacob Ide <jacobmeide@gmail.com>
2020-05-01 03:35:50 +0000

    7,1

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
 
     l = len(sys.argv)
     if l != 4:
-        print ("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
 
     a = int(sys.argv[1])
