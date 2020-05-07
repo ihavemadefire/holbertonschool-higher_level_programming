340c2771c1ca7dde5b108e1be69edbebe142d6d4
Jacob Ide <jacobmeide@gmail.com>
2020-05-07 16:46:14 +0000

    0,1

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    result = [list(map(lambda x: x ** 2, i)) for i in matrix]
-    return (result)
+    return([list(map(lambda x: x ** 2, i)) for i in matrix])
