67146f4cedeef31de045dd6b05b930fd08a0c1e7
Jacob Ide <jacobmeide@gmail.com>
2020-05-11 00:57:42 +0000

    100,0o

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,24 @@
+#include <Python.h>
+/**
+ * print_python_list_info - prints python list info
+ * @p: pyobject
+ */
+void print_python_list_info(PyObject *p)
+{
+	int size, alloc, i;
+	PyObject *r;
+
+	size = Py_SIZE(p);
+	/*cast PyObject to PyList Object*/
+	alloc = ((PyListObject *)p)->allocated;
+	/*Print header info for list object*/
+	printf("[*] Size of the Python List = %d", size);
+	printf("[*] Size of the Python List = %d", alloc);
+	/*Print each object for list*/
+	for (i = 0; i < size; i++)
+	{
+		printf("Element %d: ", i);
+		r = PyList_GetItem(p, i);
+		printf("%s\n", Py_TYPE(r)->tp_name);
+	}
+}
