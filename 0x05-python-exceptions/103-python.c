2b11822e16d67ca8cff212a7ee808e7301501844
Jacob Ide <jacobmeide@gmail.com>
2020-05-19 04:43:26 +0000

    103,0

--- 0x05-python-exceptions/103-python.c
+++ 0x05-python-exceptions/103-python.c
@@ -4,14 +4,26 @@ void print_python_list(PyObject *p);
 void print_python_bytes(PyObject *p);
 void print_python_float(PyObject *p);
 
+/**
+ * print_python_list - print python list info
+ * @p: pointer to object
+ */
 void print_python_list(__attribute__((unused))PyObject *p)
 {
 
 }
+/**
+ * print_python_bytes - print python list info
+ * @p: pointer to object
+ */
 void print_python_bytes(__attribute__((unused))PyObject *p)
 {
 
 }
+/**
+ * print_python_float - print python list info
+ * @p: pointer to object
+ */
 void print_python_float(__attribute__((unused))PyObject *p)
 {
 
