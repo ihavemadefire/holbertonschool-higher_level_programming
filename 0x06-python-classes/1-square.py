6fa22693c473568f1f46575653e507badbeb0d3e
Jacob Ide <jacobmeide@gmail.com>
2020-05-20 02:28:34 +0000

    1,1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,13 @@
 #!/usr/bin/python3
+"""Define a class Square."""
+
+
 class Square:
+    """Represent the attributes of a square."""""
     def __init__(self, size):
+        """Init a new instance of Square given a size.
+
+        Args:
+            size (int): Size of one side of the square.
+        """
         self.__size = size
