be12d0da64d299ea006527300f0805d53f7f4f1b
Jacob Ide <jacobmeide@gmail.com>
2020-05-20 03:56:02 +0000

    101,1

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -1,15 +1,20 @@
 #!/usr/bin/python3
+"""Define a class Square."""
+
+
 class Square:
-    """A square"""
+    """Represent the attributes of a square."""
     def __init__(self, size=0, position=(0, 0)):
         self.size = size
         self.position = position
 
     def area(self):
+        """Returns the area of a square."""
         return self.__size ** 2
 
     @property
     def size(self):
+        """Returns the area of a square."""
         return self.__size
 
     @size.setter
@@ -22,6 +27,7 @@ class Square:
 
     @property
     def position(self):
+        """Returns the area of a square."""
         return self.__position
 
     @position.setter
@@ -35,6 +41,7 @@ class Square:
         self.__position = value
 
     def my_print(self):
+        """Custom print function"""
         for x in range(self.__position[1]):
             print()
         for row in range(self.__size):
@@ -45,12 +52,13 @@ class Square:
             print()
 
     def __str__(self):
+        """Custom print representaion"""
         if self.__size != 0:
             for x in range(self.__position[1]):
                 print()
         for row in range(self.__size):
             [print(" ", end='') for y in range(self.__position[0])]
             [print("#", end='') for column in range(self.__size)]
-            if row != self.__size -1:
+            if row != self.__size - 1:
                 print()
         return ""
