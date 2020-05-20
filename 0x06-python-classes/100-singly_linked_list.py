0d490d42a961e98a121edd9f54514a7f75ee62d2
Jacob Ide <jacobmeide@gmail.com>
2020-05-20 18:30:51 +0000

    100,2

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -21,7 +21,7 @@ class Node:
     @data.setter
     def data(self, value):
         if not isinstance(value, int):
-            raise TypeError("size must be an integer")
+            raise TypeError("data must be an integer")
         self.__data = value
 
     @property
