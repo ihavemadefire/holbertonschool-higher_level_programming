46e6fbcabdbb7407eb9119d8e7b750a40a74affe
Jacob Ide <jacobmeide@gmail.com>
2020-05-20 02:54:31 +0000

    5,1

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,14 +1,23 @@
 #!/usr/bin/python3
+"""Define a class Square."""
+
+
 class Square:
-    """A square"""
+     """Represent the attributes of a square."""
     def __init__(self, size=0):
+         """Init a new instance of Square given a size.
+        Args:
+            size (int): Size of one side of the square.
+        """
         self.size = size
 
     def area(self):
+        """Returns the area of a square."""
         return self.__size ** 2
 
     @property
     def size(self):
+        """Getter and setter for size."""
         return self.__size
 
     @size.setter
@@ -20,6 +29,7 @@ class Square:
         self.__size = value
 
     def my_print(self):
+        """A custom print function."""
         for row in range(self.__size):
             [print("#", end='') for column in range(self.__size)]
             print()
