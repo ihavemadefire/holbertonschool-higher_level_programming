933300451bb6d18907f239a87cb35704cd621d0e
Jacob Ide <jacobmeide@gmail.com>
2020-05-21 01:23:11 +0000

    103, 0

---
+++ 0x06-python-classes/103-magic_class.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Defines a module magic_class."""
+
+import math
+
+class MagicClass:
+    """Define a class for a circle
+
+    """
+    self.__radius = 0
+    def __init__(self, radius):
+        if type(radius) is not int and type(radius) is not float:
+            raise TypeError ('radius must be a number')
+        else:
+            self.__radius = radius
+
+    def area(self):
+        """Returns the area of a circle"""
+        return self.__radius ** 2 * math.pi
+
+    def circumpherence(self):
+        """Returns the circumferance of a circle"""
+        return 2 * math.pi * self.__radius
