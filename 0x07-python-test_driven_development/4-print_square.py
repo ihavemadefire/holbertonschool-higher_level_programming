bf68c468a578965743955c90f2fc01dd1518e709
Jacob Ide <jacobmeide@gmail.com>
2020-05-23 04:05:05 +0000

    4,0

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+"""This module contains a function called Print_Square."""
+
+
+def print_square(size):
+    """This function prints squares."""
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    elif size < 0:
+        raise ValueError("size must be >= 0")
+
+    if size == 0:
+        print()
+    else:
+        for row in range(size):
+            [print("#", end='') for column in range(size)]
+            print()
