1291d48ec7b53e4315b3dbb04f601dc758cffb85
Jacob Ide <jacobmeide@gmail.com>
2020-06-02 23:51:41 +0000

    6,0

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -4,8 +4,6 @@
 
 class BaseGeometry():
     """This is the class BaseGeometry"""
-
-
     def area(self):
         """This is the area function"""
         raise Exception("area() is not implemented")
