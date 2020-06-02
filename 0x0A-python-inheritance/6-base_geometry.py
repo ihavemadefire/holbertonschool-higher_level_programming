9cd0522ab482a105c0acaa3dd27069267bc2273c
Jacob Ide <jacobmeide@gmail.com>
2020-06-02 19:51:29 +0000

    6,0

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""This module contains the class BaseGeometry"""
+
+
+class BaseGeometry():
+    """This is the class BaseGeometry"""
+
+
+    def area(self):
+        """This is the area function"""
+        raise Exception("area() is not implemented")
