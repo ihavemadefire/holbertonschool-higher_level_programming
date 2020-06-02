3521056d75506d336b06aff28865d7a26307f628
Jacob Ide <jacobmeide@gmail.com>
2020-06-02 19:04:28 +0000

    3, 0

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""This module also contains the things"""
+
+
+def is_kind_of_class(obj, a_class):
+    """This will return inhearated equivalence"""
+    return isinstance(obj, a_class)
