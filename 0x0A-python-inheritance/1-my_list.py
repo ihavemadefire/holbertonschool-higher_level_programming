ca9fe7caa2854b77765f5cec04c765f18e38f1dc
Jacob Ide <jacobmeide@gmail.com>
2020-06-03 00:56:13 +0000

    1,1

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -4,9 +4,6 @@
 
 class MyList(list):
     """This is a subclass of list"""
-    def __init__(self):
-        """Initialization function"""
-        super().__init__()
 
     def print_sorted(self):
         """Sorted print method"""
