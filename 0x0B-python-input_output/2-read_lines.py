f4e95622b85d47f12358af8d81040186621966fe
Jacob Ide <jacobmeide@gmail.com>
2020-06-03 16:49:53 +0000

    2,0

---
+++ 0x0B-python-input_output/2-read_lines.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""This module contains a single function"""
+
+
+def read_lines(filename="", nb_lines=0):
+    """This file reads a file for a certain number of lines"""
+    lines = number_of_lines(filename)
+    if nb_lines <= 0 or nb_lines > lines:
+        nb_lines = lines
+    with open(filename) as file:
+        for i in range(nb_lines):
+            print(file.readline(), end='')
+
+def number_of_lines(filename=""):
+    """This file returns a count of lines in a file"""
+    count = 0
+    with open(filename) as file:
+        for line in file:
+            count += 1
+    return count
