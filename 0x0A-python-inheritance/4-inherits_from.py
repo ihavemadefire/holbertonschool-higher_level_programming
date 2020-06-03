2a60d482f0da292391fd801a8c5ff3b300711fbf
Jacob Ide <jacobmeide@gmail.com>
2020-06-03 01:28:49 +0000

    4,3

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -4,7 +4,7 @@
 
 def inherits_from(obj, a_class):
     """detemines if subclass only"""
-    if issubclass(type(obj), a_class):
+    if issubclass(type(obj), a_class) and type(obj) != a_class:
         return True
     else:
         return False
