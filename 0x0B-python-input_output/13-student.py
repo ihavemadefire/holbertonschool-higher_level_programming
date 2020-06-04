c8727c3198b13469595cc7a749bb4638dd952ae3
Jacob Ide <jacobmeide@gmail.com>
2020-06-04 00:59:12 +0000

    13,0

---
+++ 0x0B-python-input_output/13-student.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""This module contains a class called Student"""
+
+
+class Student():
+    """this class is called student"""
+    def __init__(self, first_name, last_name, age):
+        """Initialization"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Returns dict"""
+        ret = {}
+        if attrs is not None:
+            for i in attrs:
+                if i in self.__dict__:
+                    ret.update({i: self.__dict__.get(i)})
+            return ret
+        else:
+            return self.__dict__
+
+    def reload_from_json(self, json):
+        """This is a docstring"""
+        self.first_name = json.get("first_name")
+        self.last_name = json.get("last_name")
+        self.age = json.get("age")
