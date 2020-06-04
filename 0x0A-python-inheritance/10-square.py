283706b2ffbb2e47cffe64fc64d20108ccac291c
Jacob Ide <jacobmeide@gmail.com>
2020-06-04 15:40:47 +0000

    10,0

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -7,4 +7,6 @@ class Square(Rectangle):
     """It's hip to be square"""
     def __init__(self, size):
         """Would you like to supersize?"""
+        self.integer_validator('size', size)
+        self.__size = size
         super().__init__(size, size)
