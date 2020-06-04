c9bef6adcafb57fcf50d82d4ac067ba06f607330
Jacob Ide <jacobmeide@gmail.com>
2020-06-04 16:25:59 +0000

    101

---
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Docstring"""
+
+
+def add_attribute(obj, name, value):
+    if '__dict__' not in dir(obj):
+        raise TypeError("can't add new attribute")
+    else:
+        setattr(obj, name, value)
