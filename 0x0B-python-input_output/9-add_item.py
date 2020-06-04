75019746dce113f33899a316881efde867ee7546
Jacob Ide <jacobmeide@gmail.com>
2020-06-04 01:49:46 +0000

    9,2

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -9,8 +9,8 @@ file = "add_item.json"
 
 try:
     object = load_from_json_file(file)
-    for i in range(1, len(sys.argv)):
-        object.append(sys.argv[i])
 except:
     object = []
+for i in range(1, len(sys.argv)):
+        object.append(sys.argv[i])
 save_to_json_file(object, file)
