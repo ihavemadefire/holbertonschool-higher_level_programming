4d8eeb958097397be7572ebdf01cd95dc44eeb39
Jacob Ide <jacobmeide@gmail.com>
2020-06-04 01:48:40 +0000

    9,1

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """This module contains no function"""
-import sys, json
+import sys
+import json
 load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
 save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
 
