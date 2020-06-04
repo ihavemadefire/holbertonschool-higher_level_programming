b434edcdbeb529033afc6947e7ea64bdab41a477
Jacob Ide <jacobmeide@gmail.com>
2020-06-04 01:46:43 +0000

    9,0

---
+++ 0x0B-python-input_output/9-add_item.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""This module contains no function"""
+import sys, json
+load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
+save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
+
+file = "add_item.json"
+
+try:
+    object = load_from_json_file(file)
+    for i in range(1, len(sys.argv)):
+        object.append(sys.argv[i])
+except:
+    object = []
+save_to_json_file(object, file)
