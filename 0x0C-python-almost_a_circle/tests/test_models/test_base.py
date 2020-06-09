62f71a9661b2f9e4438383acc0dff2baec3b97e8
Jacob Ide <jacobmeide@gmail.com>
2020-06-09 19:35:53 +0000

    Push everything in the tests

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -2,6 +2,8 @@
 """Unittests for Base class"""
 import unittest
 from models.base import Base
+from models.rectangle import Rectangle
+from models.square import Square
 
 class TestBaseClass(unittest.TestCase):
     """Test defs for base class"""
@@ -21,6 +23,29 @@ class TestBaseClass(unittest.TestCase):
         """Normal ordered list"""
         b1 = Base(42)
         self.assertEqual(b1.id, 42)
+
+class TestRectangleClass(unittest.TestCase):
+    """Basic tests for the rectangle class"""
+
+    def test_normal(self):
+        """Two passed arguments"""
+        r1 = Rectangle(10, 2)
+        self.assertEqual(r1.id, 4)
+        self.assertEqual(r1.width, 10)
+        self.assertEqual(r1.height, 2)
+
+    def test_optionals(self):
+        """5 passed arguments"""
+        r2 = Rectangle(10, 2, 0, 0, 12)
+        self.assertEqual(r2.width, 10)
+        self.assertEqual(r2.height, 2)
+        self.assertEqual(r2.x, 0)
+        self.assertEqual(r2.y, 0)
+        self.assertEqual(r2.id, 12)
+
+    def test_errors1(self):
+        """Tests for no args"""
+        self.assertRaises(ExpectedException, Rectangle)
 
 if __name__ == '__main__':
     unittest.main()
