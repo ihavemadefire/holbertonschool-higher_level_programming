5af8eaa032ae32faf8bbc47df236d0ea05aba9f2
Jacob Ide <jacobmeide@gmail.com>
2020-07-14 18:33:23 +0000

    0

---
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,8 @@
+--  returns all genres of DEXTER
+SELECT tv_genres.name AS genre
+FROM tv_show_genres
+LEFT JOIN tv_shows
+ON tv_show_genres.show_id = tv_shows.id
+LEFT JOIN tv_genres
+ON tv_show_genres.genre_id = tv_genres.id
+WHERE tv_shows.title = "DEXTER"
