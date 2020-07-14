df6b0fa0cee2bd3abd699ef5942d5a5f389e145e
Jacob Ide <jacobmeide@gmail.com>
2020-07-14 22:18:57 +0000

    0

---
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -0,0 +1,16 @@
+-- lists all genres not linked to the show Dexter
+SELECT DISTINCT tv_shows.title
+FROM tv_shows
+LEFT JOIN tv_show_genres
+ON tv_show_genres.show_id = tv_shows.id
+LEFT JOIN tv_genres
+ON tv_show_genres.genre_id = tv_genres.id
+WHERE tv_shows.title NOT IN (
+      SELECT tv_shows.title
+      FROM tv_show_genres
+      LEFT JOIN tv_shows
+      ON tv_show_genres.show_id = tv_shows.id
+      LEFT JOIN tv_genres
+      ON tv_show_genres.genre_id = tv_genres.id
+      WHERE tv_genres.name = "Comedy")
+ORDER BY tv_shows.title;
