9e24487fd0f353def2dc69491992830655e19c09
Jacob Ide <jacobmeide@gmail.com>
2020-08-07 19:41:41 +0000

    0

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+import MySQLdb
+import sys
+
+args_list = sys.argv[1:]
+HOST = '127.0.0.1'
+USER = args_list[0]
+PASS= args_list[1]
+MY_DB = args_list[2]
+
+if __name__ =='__main__':
+    db = MySQLdb.connect(host=HOST, user=USER, passwd=PASS, db=MY_DB)
+    cur = db.cursor()
+    states = cur.execute("SELECT * FROM cities ORDER BY id")
+    rows = cur.fetchall()
+    for row in rows:
+        print (row)
+    cur.close()
+    db.close()
