ff09924bd80bd645d0f93b1ee22580966d0247b5
Jacob Ide <jacobmeide@gmail.com>
2020-08-08 20:26:46 +0000

    0

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,22 +1,34 @@
 #!/usr/bin/python3
+"""DOCSTRING"""
+
+
 import MySQLdb
 import sys
 
 args_list = sys.argv[1:]
 HOST = '127.0.0.1'
 USER = args_list[0]
-PASS= args_list[1]
+PASS = args_list[1]
 MY_DB = args_list[2]
 NAME = args_list[3]
 
-if __name__ =='__main__':
+if __name__ == '__main__':
+
+    args_list = sys.argv[1:]
+    HOST = '127.0.0.1'
+    USER = args_list[0]
+    PASS = args_list[1]
+    MY_DB = args_list[2]
+    NAME = args_list[3]
     db = MySQLdb.connect(host=HOST, user=USER, passwd=PASS, db=MY_DB)
     cur = db.cursor()
-    states = cur.execute("SELECT cities.name FROM cities LEFT JOIN states ON states.id = cities.state_id WHERE states.name ='{}'  ORDER BY cities.id".format(NAME))
+    states = cur.execute("SELECT cities.name FROM cities LEFT JOIN states\
+    ON states.id=cities.state_id WHERE states.name='{}' ORDER\
+    BY cities.id".format(NAME))
     rows = cur.fetchall()
-    print (rows[0][0], end=" ")
+    print(rows[0][0], end=" ")
     for row in rows[1:]:
-        print (", {}".format(row[0]), end="")
-    print ()
+        print(", {}".format(row[0]), end="")
+    print()
     cur.close()
     db.close()
