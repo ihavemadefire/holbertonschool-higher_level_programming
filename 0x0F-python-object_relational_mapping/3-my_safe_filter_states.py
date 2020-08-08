77ef5b7a2cef89a9d027875910d3449aaa6b7539
Jacob Ide <jacobmeide@gmail.com>
2020-08-08 18:53:27 +0000

    0

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,19 +1,22 @@
 #!/usr/bin/python3
+"""DOCSTRING"""
 import MySQLdb
 import sys
 
-args_list = sys.argv[1:]
-HOST = '127.0.0.1'
-USER = args_list[0]
-PASS= args_list[1]
-MY_DB = args_list[2]
-I = args_list[3]
-if __name__ =='__main__':
+if __name__ == '__main__':
+
+    args_list = sys.argv[1:]
+    HOST = '127.0.0.1'
+    USER = args_list[0]
+    PASS = args_list[1]
+    MY_DB = args_list[2]
+    I = args_list[3]
     db = MySQLdb.connect(host=HOST, user=USER, passwd=PASS, db=MY_DB)
     cur = db.cursor()
-    states = cur.execute("SELECT * FROM states WHERE name=%s ORDER BY id", (I,))
+    states = cur.execute(
+        "SELECT * FROM states WHERE name=%s ORDER BY id", (I,))
     rows = cur.fetchall()
     for row in rows:
-        print (row)
+        print(row)
     cur.close()
     db.close()
