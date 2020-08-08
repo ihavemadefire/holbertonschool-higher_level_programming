b5abe13fcd64d95ceed3a6752e2f2ac52ca15b65
Jacob Ide <jacobmeide@gmail.com>
2020-08-08 23:28:38 +0000

    0

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""This module creates a select all function"""
+import sys
+from sqlalchemy.sql import select
+from sqlalchemy.sql.expression import update
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format
+                           (sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    from sqlalchemy.orm import sessionmaker
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    NM = session.query(State).get(2)
+    NM.name = "New Mexico"
+    session.commit()
