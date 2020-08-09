d4d2271b2534005c9b457ccf2ea311bc3bdba43d
Jacob Ide <jacobmeide@gmail.com>
2020-08-09 00:41:43 +0000

    0

---
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""This module creates a select all function"""
+import sys
+from sqlalchemy.sql import select
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format
+                           (sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    from sqlalchemy.orm import sessionmaker
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    result = session.query(State).filter(State.name.like("%a%"))
+    for i in result:
+        session.delete(i)
+    session.commit()
