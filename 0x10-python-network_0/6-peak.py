f2434e4dc744eb727b15535bd9e4af5268534dea
Jacob Ide <jacobmeide@gmail.com>
2020-08-18 20:39:01 +0000

    0

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""This returns a peak"""
+
+
+def find_peak(list_of_integers):
+    """Returns a list"""
+    a = list_of_integers
+    l = len(list_of_integers)
+    same = 0
+    if l == 0:
+        return None
+    if l == 1:
+        return a[0]
+    if l == 2:
+        return max(a)
+    for i in range(1, l - 1):
+        if a[i] != a[i + 1]:
+            same = 1
+        if a[i - 1] < a[i] and a[i + 1] < a[i]:
+            return a[i]
+    if same == 0:
+        return a[0]
+    return None
