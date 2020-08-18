18d29cb056be0f500a85ae6252d381ab55e90773
Jacob Ide <jacobmeide@gmail.com>
2020-08-18 19:57:20 +0000

    3

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -2,4 +2,4 @@
 # Sends a POST curl req and prints the response
 
 ES="email=hr@holbertonschool.com&subject=I will always be here for PLD"
-curl -d "$ES" -sX "POST" "$1"
+curl -s -d "$ES" -X POST "$1"
