454fa0346e47c1d1dd197c78a7730986e0e795e1
Jacob Ide <jacobmeide@gmail.com>
2020-08-24 20:18:46 +0000

    0

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Let's play with the requests module"""
+
+import requests
+
+if __name__ == "__main__":
+    http = requests.get("https://intranet.hbtn.io/status")
+    print("Body response:")
+    print("\t - type: {}".format(type(http.text)))
+    print("\t - content: {}".format(http.text))
