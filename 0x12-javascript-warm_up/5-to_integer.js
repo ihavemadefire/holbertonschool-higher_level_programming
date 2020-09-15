83f1fc38c9bd986255d4875e7e5893cfc29eafe3
Jacob Ide <jacobmeide@gmail.com>
2020-09-15 19:43:32 +0000

    add int parsing

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -3,5 +3,5 @@ const nan = isNaN(process.argv[2]);
 if (nan === true) {
   console.log('Not a number');
 } else {
-  console.log('My Number : ' + process.argv[2]);
+  console.log('My Number: ' + parseInt(process.argv[2], 10));
 }
