19590d0092bc00dafa7cb91d47ef489279be6fa8
Jacob Ide <jacobmeide@gmail.com>
2020-09-15 19:44:40 +0000

    fix output capitalization

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -3,5 +3,5 @@ const nan = isNaN(process.argv[2]);
 if (nan === true) {
   console.log('Not a number');
 } else {
-  console.log('My Number: ' + parseInt(process.argv[2], 10));
+  console.log('My number: ' + parseInt(process.argv[2], 10));
 }
