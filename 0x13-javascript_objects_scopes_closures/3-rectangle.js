0eb1f0132d5a36b3e1012cefd38e71987bb98a68
Jacob Ide <jacobmeide@gmail.com>
2020-09-21 14:50:23 +0000

    initial push

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+module.exports = class Rectangle {
+  constructor (width, height) {
+    if (width >= 1 && height >= 1) {
+      this.width = width;
+      this.height = height;
+    }
+  }
+
+  print () {
+    for (let i = 0; i <= this.height; i++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+};
