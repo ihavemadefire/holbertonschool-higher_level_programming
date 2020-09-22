6e66c4bd9739cb3b95c1896951081d70eda216fb
Jacob Ide <jacobmeide@gmail.com>
2020-09-22 17:35:59 +0000

    correct utf8 specification

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const args = (process.argv);
 const fs = require('fs');
-fs.readFile(args[2], 'utf-8', function (error, content) {
+fs.readFile(args[2], 'utf8', function (error, content) {
   console.log(content || error);
 });
