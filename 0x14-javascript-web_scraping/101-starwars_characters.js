5da61ea15b4d83e21a47d9844f56a085576aa968
Jacob Ide <jacobmeide@gmail.com>
2020-09-23 03:15:01 +0000

    recursive solution. Initial push

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -5,10 +5,16 @@ const url = 'https://swapi-api.hbtn.io/api/films/' + args[2];
 request(url, function (error, response, body) {
   if (!error) {
     const chars = JSON.parse(body).characters;
-    chars.map(x => request(x, function (error, response, body) {
-      if (!error) {
-        console.log(JSON.parse(body).name);
-      }
-    }));
+    printFunc(chars, 0);
   }
 });
+function printFunc (chars, i) {
+  request(chars[i], function (error, response, body) {
+    if (!error) {
+      console.log(JSON.parse(body).name);
+      if (i + 1 < chars.length) {
+        printFunc(chars, i + 1);
+      }
+    }
+  });
+}
