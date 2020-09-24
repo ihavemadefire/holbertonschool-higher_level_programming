29d450c278299077bbe71d78ef3bafe7a7e21fec
Jacob Ide <jacobmeide@gmail.com>
2020-09-24 12:46:39 -0500

    take out console log

--- 0x15-javascript-web_jquery/7-script.js
+++ 0x15-javascript-web_jquery/7-script.js
@@ -2,7 +2,6 @@
 $(document).ready(function () {
   const url = 'https://swapi-api.hbtn.io/api/people/5/?format=json';
   $.get(url, function (data) {
-    console.log(data.name);
     $('#character').text(data.name);
   });
 });
