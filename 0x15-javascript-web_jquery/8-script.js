d24fcdff86266c2478430b2ce6bbfd75103239ef
Jacob Ide <jacobmeide@gmail.com>
2020-09-24 13:08:27 -0500

    initial commit

--- 0x15-javascript-web_jquery/8-script.js
+++ 0x15-javascript-web_jquery/8-script.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+$(document).ready(function () {
+  const url = 'https://swapi-api.hbtn.io/api/films/?format=json';
+  $.get(url, function (data) {
+    data.results.map(
+      x => { $('#list_movies').append(`<li>${x.title}</li>`); }
+    );
+  }
+  );
+});
